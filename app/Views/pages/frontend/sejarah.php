97236cb5371fe8202b4ff04692d6ec3bf52f0a1f
Nindia Oktapia <148666929+Viary17@users.noreply.github.com>
2023-12-22 21:29:20 +0700

    Create sejarah.php

---
+++ app/Views/pages/frontend/sejarah.php
@@ -0,0 +1,49 @@
+<?= $this->extend('layouts/frontend/base_layouts') ?>
+<?= $this->section('title') ?>Berita<?= $this->endSection() ?>
+<?= $this->section('content') ?>
+<div class="container">
+        <div class="sambutan-container">
+            <h2 class="text-center mb-4">Sejarah Smpn 3 Subang</h2>
+            <center><img src="img/logo.png" alt="logo" class="profile-image mb-5">
+            </center>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore sed in excepturi? Iure, officia fuga
+                reiciendis, obcaecati eligendi aliquam laborum voluptatibus officiis atque consectetur sint optio ipsum
+                unde vel doloremque asperiores assumenda labore quod accusantium dignissimos quasi! Sed voluptas unde
+                deleniti, necessitatibus facilis, similique amet, quae nemo fugit illo officia sint! Perferendis vitae,
+                voluptatem id odio doloribus, laboriosam quia explicabo nisi ullam beatae natus ea qui a, sapiente
+                recusandae. Exercitationem architecto maiores, quisquam ut dicta rerum nisi. Pariatur reiciendis illo
+                impedit eaque placeat. Molestias non perferendis sunt nemo. Ea obcaecati enim sapiente deserunt iste
+                expedita amet illum quisquam rem officia eaque vel deleniti architecto, corrupti, numquam recusandae.
+                Placeat reprehenderit suscipit error vero perspiciatis officiis enim doloremque at veniam in voluptas
+                dolorum veritatis maxime, iure ipsam nobis accusantium, et, ratione similique ut quos aliquam tempora.
+                Dolores nemo beatae minus exercitationem eos perspiciatis officia blanditiis reiciendis? Fugit, minima?
+                Sapiente labore perspiciatis dolore, sed commodi, placeat a assumenda possimus est maxime asperiores,
+                eaque sequi cum quisquam explicabo corporis ad et quaerat voluptas dicta ducimus. Magnam,
+                necessitatibus! A deleniti qui quae iure vel quidem sed dolorum unde modi voluptatem numquam dolore
+                tempore soluta ipsa reprehenderit natus enim veniam dolorem possimus, aut dolores. Adipisci ea magni
+                laboriosam maiores fugit, beatae doloribus eius accusantium, quos autem tenetur iste consequatur,
+                aliquam qui? Neque, tempora assumenda quas voluptatem mollitia distinctio consequuntur adipisci nam
+                libero delectus praesentium blanditiis corrupti in hic repellat. Deserunt officia atque quod incidunt
+                aliquid ullam at. Error, corporis excepturi sit molestiae harum dolorum soluta, eius deleniti dolore
+                expedita, explicabo ducimus labore aliquam debitis? Repudiandae qui, quaerat totam adipisci illum iusto,
+                fuga facilis aut deserunt non, accusamus reiciendis corporis! Eum, explicabo sint minus consectetur
+                mollitia, accusantium nostrum exercitationem, porro iusto sunt corporis sapiente! Voluptate magnam
+                quibusdam quo. Deserunt vero temporibus blanditiis. Nihil hic nulla eveniet voluptatum assumenda, cum
+                porro delectus perspiciatis velit, quia ut id voluptate aliquam nisi soluta aspernatur ipsam? Est optio
+                deserunt tempora earum, id blanditiis vero dignissimos debitis velit at! Asperiores quidem praesentium
+                quas perferendis nam iste modi voluptatum aliquam exercitationem tempore eligendi, corrupti consectetur?
+                Vitae alias dignissimos numquam ipsum ducimus deserunt perspiciatis expedita accusamus facere saepe at
+                dolor, cum architecto error tenetur nostrum laborum doloribus ratione reiciendis est, ipsam nulla
+                pariatur? Quibusdam eaque veniam, rem velit consequuntur deserunt harum officiis necessitatibus ex ipsam
+                voluptate quos impedit repellat exercitationem iure, earum reiciendis sequi ducimus placeat hic,
+                temporibus facere nostrum nesciunt. Suscipit error nihil reprehenderit saepe id quis eum odio
+                exercitationem voluptatem totam debitis facere, vitae quia nisi praesentium inventore sint ad quas ex
+                amet officia voluptate, corrupti, molestiae dolores. Accusamus natus enim nobis ratione. Labore,
+                molestias fugiat suscipit, iusto fuga placeat nihil sint eos totam accusantium assumenda quos. Ipsum
+                dolorem eum qui? Omnis, quisquam ex consectetur sed ducimus quo vero temporibus unde, tenetur
+                voluptatibus mollitia quam nostrum consequuntur ad quia dicta beatae ratione debitis? Obcaecati ratione
+                nisi totam sed omnis, minima dolorem iusto reprehenderit at rerum debitis optio recusandae animi
+                sapiente itaque! Velit earum asperiores aliquid voluptas, facilis aut tempore tenetur culpa assumenda!
+            </p>
+        </div>
+    </div>

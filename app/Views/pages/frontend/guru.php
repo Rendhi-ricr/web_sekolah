b085a873fe9d533d0909499325980dc5d9ab0df8
Nindia Oktapia <148666929+Viary17@users.noreply.github.com>
2023-12-22 21:23:30 +0700

    Create guru.php

---
+++ app/Views/pages/frontend/guru.php
@@ -0,0 +1,44 @@
+<?= $this->extend('layouts/frontend/base_layouts') ?>
+<?= $this->section('title') ?>Berita<?= $this->endSection() ?>
+<?= $this->section('content') ?>
+<div class="container mt-5">
+        <h2 class="text-center mb-5">Data Guru</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="teacher-card">
+                    <img src="img/guru1.jpg" alt="Teacher 1" class="teacher-image">
+                    <div class="teacher-name">windi asksadk aksjdbakjb</div>
+                    <div class="teacher-subject">MTK</div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="teacher-card">
+                    <img src="img/guru2.jfif" alt="Teacher 2" class="teacher-image">
+                    <div class="teacher-name">Nama Guru 2</div>
+                    <div class="teacher-subject">Mata Pelajaran 2</div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="teacher-card">
+                    <img src="img/guru3.jfif" alt="Teacher 2" class="teacher-image">
+                    <div class="teacher-name">Nama Guru 2</div>
+                    <div class="teacher-subject">Mata Pelajaran 2</div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="teacher-card">
+                    <img src="img/guru4.jfif" alt="Teacher 2" class="teacher-image">
+                    <div class="teacher-name">Nama Guru 2</div>
+                    <div class="teacher-subject">Mata Pelajaran 2</div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="teacher-card">
+                    <img src="img/guru5.jfif" alt="Teacher 2" class="teacher-image">
+                    <div class="teacher-name">Nama Guru 2</div>
+                    <div class="teacher-subject">Mata Pelajaran 2</div>
+                </div>
+            </div>
+        </div>
+    </div>

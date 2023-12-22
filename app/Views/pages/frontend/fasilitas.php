9298f776456847317afe8eb925056e35cf24a54c
Nindia Oktapia <148666929+Viary17@users.noreply.github.com>
2023-12-22 21:20:11 +0700

    Create fasilitas.php

---
+++ app/Views/pages/frontend/fasilitas.php
@@ -0,0 +1,160 @@
+<?= $this->extend('layouts/frontend/base_layouts') ?>
+<?= $this->section('title') ?>Fasilitas<?= $this->endSection() ?>
+<?= $this->section('content') ?>
+<div class="container">
+        <h2 class="text-center mb-4 mt-3">Fasilitas</h2>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f1.png" class="card-img-top" alt="fasilitas 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Ruang Kepala Sekolah</h5>
+                        <p class="card-text">Ruang Kepala Sekolah merupakan ruangan khusus yang menjadi tempat kerja dan
+                            pusat pengambilan keputusan oleh Kepala Sekolah. Ruang ini memiliki peran penting dalam
+                            mengelola administrasi, berkomunikasi dengan staf, guru, orangtua siswa, dan pihak terkait,
+                            serta memimpin berbagai kegiatan sekolah.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f2.png" class="card-img-top" alt="Ekstrakurikuler 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Ruang Guru</h5>
+                        <p class="card-text">Ruang Guru merupan ruang khusus yang diperuntukkan bagi para guru untuk
+                            melaksanakan tugas-tugas pengajaran, perencanaan pembelajaran, dan berbagai kegiatan terkait
+                            pendidikan. Ruang ini seringkali merupakan tempat di mana guru-guru berkumpul untuk
+                            berdiskusi, berkolaborasi, dan merencanakan aktivitas pengajaran.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f3.png" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Ruang Tata Usaha</h5>
+                        <p class="card-text">Ruang Tata Usaha (TU) adalah area yang berfungsi sebagai pusat administrasi
+                            dan manajemen. Ruang ini menjadi tempat berlangsungnya berbagai kegiatan administratif yang
+                            melibatkan staf tata usaha dan pelayanan terkait.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f4.png" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Ruang BK</h5>
+                        <p class="card-text">Ruang Bimbingan dan Konseling (BK) merupakan area yang didedikasikan untuk
+                            kegiatan bimbingan dan konseling. Ruang ini bertujuan untuk memberikan layanan bimbingan dan
+                            konseling kepada siswa SMPN 3 Subang agar dapat mengembangkan potensi, mengatasi masalah
+                            pribadi, sosial, dan akademis, serta membuat keputusan yang baik terkait perkembangan
+                            mereka.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f5.jpg" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Ruang UKS</h5>
+                        <p class="card-text">Ruang UKS (Unit Kesehatan Sekolah) merupakan ruang yang dikhususkan untuk
+                            kegiatan-kegiatan terkait kesehatan dan kesejahteraan siswa. Tujuan dari ruang UKS adalah
+                            untuk mendukung upaya promosi kesehatan, pencegahan penyakit, dan memberikan layanan
+                            kesehatan dasar kepada siswa.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f6.jpg" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Perpustakaan</h5>
+                        <p class="card-text">Perpustakaan merupakan tempat yang menyimpan berbagai sumber daya bacaan
+                            dan materi pembelajaran untuk digunakan oleh siswa dan staf sekolah. Siswa dan staf juga
+                            diperbolehkan meminjamnya dengan batas waktu yang ditentukan.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f7.jpg" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Koperasi</h5>
+                        <p class="card-text">Koperasi menyediakan barang atau layanan yang dibutuhkan siswa dan staf
+                            SMPN 3 Subang. Fasilitas yang umumnya terdapat di koperasi SMPN 3 Subang meliputi penjualan
+                            buku, alat tulis, seragam sekolah, perlengkapan olahraga, serta makanan dan minuman ringan.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f8.jpg" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Lab. Komputer</h5>
+                        <p class="card-text">Lab komputer disediakan bagi siswa/siswi SMPN 3 Subang untuk belajar,
+                            berlatih, dan mengembangkan keterampilan komputer.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f9.jpg" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Lab IPA Biologi</h5>
+                        <p class="card-text">Lab IPA Biologi disediakan bagi siswa/siswi SMPN 3 Subang untuk kegiatan
+                            praktikum dan eksperimen dalam mata pelajaran Ilmu Pengetahuan Alam (IPA) khususnya biologi.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f10.png" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Masjid</h5>
+                        <p class="card-text">Masjid merupakan sarana bagi siswa, guru, dan staf sekolah untuk
+                            menjalankan ibadah sholat, membaca Al-Qur'an, dan melaksanakan kegiatan keagamaan lainnya.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f11.jpg" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Kantin</h5>
+                        <p class="card-text">Kantin menyediakan makanan dan minuman bagi siswa, guru, dan staf SMPN 3
+                            Subang.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f12.jpg" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">WC</h5>
+                        <p class="card-text">WC atau toilet dibangun untuk memberikan tempat bagi siswa, guru, dan staf
+                            SMPN 3 Subang untuk melakukan kegiatan buang air kecil dan buang air besar.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card ekstrakurikuler-card">
+                    <img src="img/fasilitas/f13.png" class="card-img-top" alt="Ekstrakurikuler 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Taman Toga</h5>
+                        <p class="card-text">Taman boga merupakan tempat untuk penanaman dan penumbuhan berbagai jenis
+                            tanaman yang ada di SMPN 3 Subang.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>

e29acf1a4259d8b984c1d33c32500d62d0206997
Cupitacantikk <153527206+Cupitacantikk@users.noreply.github.com>
2023-12-22 21:26:25 +0700

    Create aagenda.php

---
+++ app/Views/pages/backend/aagenda.php
@@ -0,0 +1,73 @@
+<section class="content">
+            <div class="content-header">
+                <h1>Data Agenda </h1>
+
+                <ol class="breadcrumb content-header-breadcrumb my-sm-auto">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                </ol>
+
+            </div>
+            <div class="main-content">
+                <div class="block">
+                    <a href="" class="btn btn-primary btn-sm my-4"><i class="bx bx-plus"></i> Tambah Data</a>
+                    <div class="card">
+                        <div class="card-header">
+                            <h6>Table</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Nama</th>
+                                            <th>Alamat</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Cupita</td>
+                                            <td>Indramayu</td>
+                                            <td>
+                                                <a href="" class="btn btn-warning btn-sm">Edit</a>
+                                                <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Tia</td>
+                                            <td>Subang</td>
+                                            <td>
+                                                <a href="" class="btn btn-warning btn-sm">Edit</a>
+                                                <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>Rendi</td>
+                                            <td>Subang</td>
+                                            <td>
+                                                <a href="" class="btn btn-warning btn-sm">Edit</a>
+                                                <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>Gan gan</td>
+                                            <td>Subang</td>
+                                            <td>
+                                                <a href="" class="btn btn-warning btn-sm">Edit</a>
+                                                <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>

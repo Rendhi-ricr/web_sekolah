2134370f72347d355a828d07ebba359692949186
Cupitacantikk <153527206+Cupitacantikk@users.noreply.github.com>
2023-12-27 21:42:59 +0700

    Update guru.php

--- app/Views/pages/frontend/guru.php
+++ app/Views/pages/frontend/guru.php
@@ -7,37 +7,37 @@
             <div class="col-md-4">
                 <div class="teacher-card">
                     <img src="img/guru1.jpg" alt="Teacher 1" class="teacher-image">
-                    <div class="teacher-name">windi asksadk aksjdbakjb</div>
-                    <div class="teacher-subject">MTK</div>
+                    <div class="teacher-name">windi astuti</div>
+                    <div class="teacher-subject">Matematika</div>
                 </div>
             </div>
 
             <div class="col-md-4">
                 <div class="teacher-card">
                     <img src="img/guru2.jfif" alt="Teacher 2" class="teacher-image">
-                    <div class="teacher-name">Nama Guru 2</div>
-                    <div class="teacher-subject">Mata Pelajaran 2</div>
+                    <div class="teacher-name">Tia Saedin</div>
+                    <div class="teacher-subject">Bahasa Indonesia</div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="teacher-card">
                     <img src="img/guru3.jfif" alt="Teacher 2" class="teacher-image">
-                    <div class="teacher-name">Nama Guru 2</div>
-                    <div class="teacher-subject">Mata Pelajaran 2</div>
+                    <div class="teacher-name">Cupita Sari</div>
+                    <div class="teacher-subject">Teknik Jaringan</div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="teacher-card">
                     <img src="img/guru4.jfif" alt="Teacher 2" class="teacher-image">
-                    <div class="teacher-name">Nama Guru 2</div>
-                    <div class="teacher-subject">Mata Pelajaran 2</div>
+                    <div class="teacher-name">Rendhi Ricardo</div>
+                    <div class="teacher-subject">Bahasa Arab</div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="teacher-card">
                     <img src="img/guru5.jfif" alt="Teacher 2" class="teacher-image">
-                    <div class="teacher-name">Nama Guru 2</div>
-                    <div class="teacher-subject">Mata Pelajaran 2</div>
+                    <div class="teacher-name">Nindia Oktapia</div>
+                    <div class="teacher-subject">Seni Budaya</div>
                 </div>
             </div>
         </div>
